405e49abd35ab54f2095044b0e29adc72372554b
Matthew Pitkin <m@ttpitk.in>
2024-03-22 09:30:10 +0000

    bump version number

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     maintainer="Matt Pitkin",
     maintainer_email="matthew.pitkin@ligo.org",
     name="md-tooltips-link",
-    version="0.2.2",
+    version="0.2.3",
     description="A Python markdown extension for implementing a glossary with tooltips",
     py_modules=["mdtooltipslink"],
     install_requires = \
